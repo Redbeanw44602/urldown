620b77b02b4c51738a487306b1d4ddae1b301a93
Redbeanw44602 <redbeana44945@gmail.com>
2024-09-05 23:44:47 +0800

    feat: add util.system

--- toolbox/util/system.py
+++ toolbox/util/system.py
@@ -9,3 +9,11 @@ def is_windows():
 def pause():
     if is_windows():
         os.system('pause')
+
+
+def get_executable_extension():
+    return '.exe' if is_windows() else ''
+
+
+def get_redirect_to_null_suffix():
+    return '> NUL 2>&1' if is_windows() else '> /dev/null 2>&1'
